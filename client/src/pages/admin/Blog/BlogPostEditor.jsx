0a83d65f8a3efdea0dcd875804ad57af6c746a03
Raj Kumar Nepal <rajkumarnepal8@gmail.com>
2026-01-03 23:12:52 +0545

    fix(client): update author dropdown to show Full Name (username)

--- client/src/pages/admin/Blog/BlogPostEditor.jsx
+++ client/src/pages/admin/Blog/BlogPostEditor.jsx
@@ -645,7 +645,7 @@ const BlogPostEditor = () => {
                                     <option value="" disabled>Select Author</option>
                                     {users.map(user => (
                                         <option key={user._id} value={String(user._id)}>
-                                            {user.username} ({user.role})
+                                            {user.fullName || user.username} ({user.username})
                                         </option>
                                     ))}
                                 </select>
